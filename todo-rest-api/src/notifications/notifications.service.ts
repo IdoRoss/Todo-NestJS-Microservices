213b301f45ee2f763f2d25c8b4cac71c28d5d0b9
IdoRoss <69646826+IdoRoss@users.noreply.github.com>
2023-06-19 18:26:33 +0300

    Change to raw axios

--- todo-rest-api/src/notifications/notifications.service.ts
+++ todo-rest-api/src/notifications/notifications.service.ts
@@ -3,18 +3,15 @@ import { CreateNotificationDto } from './dto/create-notification.dto';
 import { UpdateNotificationDto } from './dto/update-notification.dto';
 import { HttpService } from '@nestjs/axios';
 import { Observable } from 'rxjs/internal/Observable';
-import { AxiosResponse } from 'axios';
+import axios, { AxiosResponse, AxiosError } from 'axios';
 
 @Injectable()
 export class NotificationsService {
   private readonly API_URL = 'http://localhost:3001/api/notifications';
-  constructor(private readonly httpService: HttpService) {}
+  constructor() {}
 
   async createNotification(createNotificationDto: CreateNotificationDto) {
-    return await this.httpService.axiosRef.post(
-      this.API_URL,
-      createNotificationDto,
-    );
+    return await axios.post(this.API_URL, createNotificationDto);
   }
 
   async updateNotification(
@@ -22,13 +19,10 @@ export class NotificationsService {
     updateNotificationDto: UpdateNotificationDto,
   ) {
     // Make API call to create a notification
-    return await this.httpService.axiosRef.post(
-      `${this.API_URL}/${id}`,
-      updateNotificationDto,
-    );
+    return await axios.post(`${this.API_URL}/${id}`, updateNotificationDto);
   }
   async deleteNotification(id: string) {
     // Make API call to create a notification
-    return await this.httpService.axiosRef.delete(`${this.API_URL}/${id}`);
+    return await axios.delete(`${this.API_URL}/${id}`);
   }
 }
