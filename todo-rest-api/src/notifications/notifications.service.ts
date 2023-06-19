b8f13f2404339c39b59dab965173360a25af7492
IdoRoss <69646826+IdoRoss@users.noreply.github.com>
2023-06-19 19:11:55 +0300

    Fix import todo api NotificationsService

--- todo-rest-api/src/notifications/notifications.service.ts
+++ todo-rest-api/src/notifications/notifications.service.ts
@@ -1,14 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { CreateNotificationDto } from './dto/create-notification.dto';
 import { UpdateNotificationDto } from './dto/update-notification.dto';
-import { HttpService } from '@nestjs/axios';
-import { Observable } from 'rxjs/internal/Observable';
-import axios, { AxiosResponse, AxiosError } from 'axios';
+
+import axios from 'axios';
 
 @Injectable()
 export class NotificationsService {
   private readonly API_URL = 'http://localhost:3001/api/notifications';
-  constructor() {}
 
   async createNotification(createNotificationDto: CreateNotificationDto) {
     return await axios.post(this.API_URL, createNotificationDto);
--- todo-rest-api/src/todo/todo.module.ts
+++ todo-rest-api/src/todo/todo.module.ts
@@ -4,10 +4,11 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { TodoService } from './todo.service';
 import { TodoController } from './todo.controller';
 import { TodoSchema } from './entities/todo.entity';
+import { NotificationsService } from 'src/notifications/notifications.service';
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: 'Todo', schema: TodoSchema }])],
   controllers: [TodoController],
-  providers: [TodoService],
+  providers: [TodoService, NotificationsService],
 })
 export class TodoModule {}
